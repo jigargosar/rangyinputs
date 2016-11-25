8f705943bb9f7f76f13dc27e558ab46c0e5e27ac
Jigar Gosar <jigar.gosar@gmail.com>
2016-11-25 17:36:24 +0530

    add UMD compatibility

--- rangyinputs-jquery-src.js
+++ rangyinputs-jquery-src.js
@@ -14,7 +14,35 @@
  * Version: 1.2.0
  * Build date: 30 November 2014
  */
-(function($) {
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function($) {
     var UNDEF = "undefined";
     var getSelection, setSelection, deleteSelectedText, deleteText, insertText;
     var replaceSelectedText, surroundSelectedText, extractSelectedText, collapseSelection;
@@ -314,4 +342,4 @@
             surroundSelectedText: jQuerify(surroundSelectedText, true)
         });
     });
-})(jQuery);
+}));
